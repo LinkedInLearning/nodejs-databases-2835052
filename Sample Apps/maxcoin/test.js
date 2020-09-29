4a3b172be3e511fe784f6253eeb8890d1421d6b1
danielkhan <daniel@khan.io>
2020-09-29 22:58:03 +0200

    initial state

--- Sample Apps/maxcoin/test.js
+++ Sample Apps/maxcoin/test.js
@@ -1,12 +1,16 @@
-// request is a module that makes http calls easier
+/* eslint-disable no-console */
 const axios = require('axios');
 
 // Generic function that fetches the closing bitcoin dates of the last month from a public API
-async function fetchFromAPI(callback) {
-    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+async function fetchFromAPI() {
+  const response = await axios.get(
+    'https://api.coindesk.com/v1/bpi/histerical/close.json'
+  );
+  return response.data;
 }
 
-fetchFromAPI((err, data) => {
-    if (err) throw err;
-    console.log(data);
+fetchFromAPI().then((response) => {
+  console.log(response);
+}).catch((error) => {
+  console.error(error.message);
 });
