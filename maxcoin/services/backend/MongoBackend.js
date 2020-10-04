d9cea22a593b0508c99557e5f9333d8025beb09e
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-05 01:15:15 +0200

    imongodb insert

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -31,7 +31,17 @@ class MongoBackend {
     return false;
   }
 
-  async insert() {}
+  async insert() {
+    const data = await this.coinAPI.fetch();
+    const documents = [];
+    Object.entries(data.bpi).forEach((entry) => {
+      documents.push({
+        date: entry[0],
+        value: entry[1],
+      });
+    });
+    return this.collection.insertMany(documents);
+  }
 
   async getMax() {}
 
@@ -45,6 +55,13 @@ class MongoBackend {
       throw new Error("Connecting to MongoDB failed");
     }
     console.timeEnd("mongodb-connect");
+
+    console.info("Inserting into MongoDB");
+    console.time("mongodb-insert");
+    const insertResult = await this.insert();
+    console.timeEnd("mongodb-insert");
+
+    console.info(`Inserted ${insertResult.result.n} documents into MongoDB`);
 
     console.info("Disconnecting from MongoDB");
     console.time("mongodb-disconnect");
