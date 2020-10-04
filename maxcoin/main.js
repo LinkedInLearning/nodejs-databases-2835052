341acfb72bb9b94db8a70bbc86b6fc4b0dc08dff
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-05 00:16:32 +0200

    mongodb connect

--- maxcoin/main.js
+++ maxcoin/main.js
@@ -1,10 +1,12 @@
-const CoinAPI = require('./services/CoinAPI');
+const MongoBackend = require("./services/backend/MongoBackend");
 
 async function run() {
-  const coinAPI = new CoinAPI();
-  return coinAPI.fetch();
+  const mongoBackend = new MongoBackend();
+  return mongoBackend.max();
 }
 
-run().then((result) => {
-  console.log(result);
-}).catch(err => console.error(err));
+run()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => console.error(err));
--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -1,30 +1,55 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
-const CoinAPI = require('../CoinAPI');
+
+const { MongoClient } = require("mongodb");
+
+const CoinAPI = require("../CoinAPI");
 
 class MongoBackend {
-
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.mongoUrl = "mongodb://localhost:37017/maxcoin";
+    this.client = null;
+    this.collection = null;
   }
 
   async connect() {
-
+    const mongoClient = new MongoClient(this.mongoUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    this.client = await mongoClient.connect();
+    this.collection = this.client.db("maxcoin").collection("values");
+    return this.client;
   }
 
   async disconnect() {
-
+    if (this.client) {
+      return this.client.close();
+    }
+    return false;
   }
 
-  async insert() {
-  }
+  async insert() {}
 
-  async getMax() {
-
-  }
+  async getMax() {}
 
   async max() {
+    console.info("Connection to MongoDB");
+    console.time("mongodb-connect");
+    const client = await this.connect();
+    if (client.isConnected()) {
+      console.info("Successfully connected to MongoDB");
+    } else {
+      throw new Error("Connecting to MongoDB failed");
+    }
+    console.timeEnd("mongodb-connect");
+
+    console.info("Disconnecting from MongoDB");
+    console.time("mongodb-disconnect");
+    await this.disconnect();
+    console.timeEnd("mongodb-disconnect");
   }
 }
 
