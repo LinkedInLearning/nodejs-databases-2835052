1794c028788637ddfdeb1d5ee169589a16d25645
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-11 12:21:10 +0200

    03_07

--- shopper/server/bin/start.js
+++ shopper/server/bin/start.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 
 const http = require('http');
+const mongoose = require('mongoose');
 
 const config = require('../config');
 const App = require('../app');
+
+async function connectToMongoose() {
+  return mongoose.connect(config.mongodb.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+}
 
 /* Logic to start the application */
 const app = App(config);
@@ -45,4 +54,9 @@ function onListening() {
 server.on('error', onError);
 server.on('listening', onListening);
 
-server.listen(port);
+connectToMongoose().then(() => {
+  console.info('Successfully connected to MongoDB');
+  server.listen(port);
+}).catch((error) => {
+  console.error(error);
+});
--- shopper/server/config/index.js
+++ shopper/server/config/index.js
@@ -2,4 +2,7 @@ const pkg = require('../../package.json');
 
 module.exports = {
   applicationName: pkg.name,
+  mongodb: {
+    url: 'mongodb://localhost:37017/shopper',
+  }
 };
