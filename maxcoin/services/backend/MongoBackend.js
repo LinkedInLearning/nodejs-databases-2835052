2e3870427390362d769f2f1dfa138c681c788eb6
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-11 19:15:41 +0200

    Update fix

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -1,22 +1,58 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
+
+const { MongoClient } = require("mongodb");
+
 const CoinAPI = require("../CoinAPI");
 
 class MongoBackend {
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.mongoUrl = "mongodb://localhost:37017/maxcoin";
+    this.client = null;
+    this.collection = null;
   }
 
-  async connect() {}
+  async connect() {
+    const mongoClient = new MongoClient(this.mongoUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    this.client = await mongoClient.connect();
+    this.collection = this.client.db("maxcoin").collection("values");
+    return this.client;
+  }
 
-  async disconnect() {}
+  async disconnect() {
+    if (this.client) {
+      return this.client.close();
+    }
+    return false;
+  }
 
-  async insert() {}
+  async insert() {
+  }
 
-  async getMax() {}
+  async getMax() {
+  }
 
-  async max() {}
+  async max() {
+    console.info("Connection to MongoDB");
+    console.time("mongodb-connect");
+    const client = await this.connect();
+    if (client.isConnected()) {
+      console.info("Successfully connected to MongoDB");
+    } else {
+      throw new Error("Connecting to MongoDB failed");
+    }
+    console.timeEnd("mongodb-connect");
+
+    console.info("Disconnecting from MongoDB");
+    console.time("mongodb-disconnect");
+    await this.disconnect();
+    console.timeEnd("mongodb-disconnect");
+  }
 }
 
 module.exports = MongoBackend;
