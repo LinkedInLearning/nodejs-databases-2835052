21b250324205b82a0dd24004cdd080382cf9e6da
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-11 19:20:37 +0200

    fix status

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -9,11 +9,27 @@ const CoinAPI = require("../CoinAPI");
 class MongoBackend {
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.mongoUrl = "mongodb://localhost:37017/maxcoin";
+    this.client = null;
+    this.collection = null;
   }
 
-  async connect() {}
+  async connect() {
+    const mongoClient = new MongoClient(this.mongoUrl, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+    this.client = await mongoClient.connect();
+    this.collection = this.client.db("maxcoin").collection("values");
+    return this.client;
+  }
 
-  async disconnect() {}
+  async disconnect() {
+    if (this.client) {
+      return this.client.close();
+    }
+    return false;
+  }
 
   async insert() {
     const data = await this.coinAPI.fetch();
