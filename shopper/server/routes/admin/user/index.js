dc9370d77fa667f288e4e4374f8d54e1175774b4
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-17 15:21:44 +0200

    revert state

--- shopper/server/routes/admin/user/index.js
+++ shopper/server/routes/admin/user/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-const UserService = require('../../../services/UserService');
-
 module.exports = () => {
   const router = express.Router();
 
-  router.get('/:userId?', async (req, res, next) => {
+  router.get('/:userId?', async (req, res) => {
+    return res.render('admin/user');
 
+    /*
     try {
       const users = await UserService.getAll();
 
@@ -23,12 +23,14 @@ module.exports = () => {
     } catch (err) {
       return next(err);
     }
-
+    */
   });
 
   // Save or update user
-  router.post('/', async (req, res) => {
+  router.post('/', async (req, res, next) => {
+    return next('Not implemented');
 
+    /*
     const email = req.body.email.trim();
     const password = req.body.password.trim();
     // Add this here because on update we might want to keep the password as it is
@@ -66,10 +68,13 @@ module.exports = () => {
       console.error(err);
       return res.redirect('/admin/user');
     }
+    */
   });
 
   // Delete user
-  router.get('/delete/:userId', async (req, res) => {
+  router.get('/delete/:userId', async (req, res, next) => {
+    return next('Not implemented');
+    /*
     try {
       await UserService.remove(req.params.userId);
     } catch (err) {
@@ -87,16 +92,20 @@ module.exports = () => {
       text: 'The user was successfully deleted!',
     });
     return res.redirect('/admin/user');
+    */
   });
 
 
-  router.get('/impersonate/:userId', (req, res) => {
+  router.get('/impersonate/:userId', (req, res, next) => {
+    return next('Not implemented');
+    /*
     req.session.userId = req.params.userId;
     req.session.messages.push({
       type: 'success',
       text: 'User successfully switched',
     });
     return res.redirect('/admin/user');
+    */
   });
 
   return router;
