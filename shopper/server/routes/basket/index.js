736b0d02ce6613e80e338901337b8d7f75e60867
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-23 23:19:32 +0200

    Basket scaffold

--- shopper/server/routes/basket/index.js
+++ shopper/server/routes/basket/index.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 
 module.exports = () => {
   const router = express.Router();
 
-  router.get('/', async (req, res) => {
-    return res.render('basket', {});
+  router.get("/", async (req, res) => {
+    return res.render("basket", {});
 
     /*
-    const basketItems = await basket.getAll(res.locals.currentUser.id);
+    const basketItems = await basket.getAll();
     let items = [];
     if (basketItems) {
-      items = await Promise.all(Object.keys(basketItems).map(async (key) => {
-        const item = await ItemService.getOne(key);
+      items = await Promise.all(Object.keys(basketItems).map(async (itemId) => {
+        const item = await ItemService.getOne(itemId);
         item.quantity = basketItems[key];
         return item;
       }));
@@ -20,8 +20,8 @@ module.exports = () => {
     */
   });
 
-  router.get('/remove/:itemId', async (req, res, next) => {
-    return next('Not implemented');
+  router.get("/remove/:itemId", async (req, res, next) => {
+    return next("Not implemented");
 
     /*
     if (!res.locals.currentUser) {
@@ -33,7 +33,7 @@ module.exports = () => {
     }
 
     try {
-      await basket.remove(req.params.itemId, res.locals.currentUser.id);
+      await basket.remove(itemId);
       req.session.messages.push({
         type: 'success',
         text: 'The item was removed from the the basket',
@@ -51,8 +51,8 @@ module.exports = () => {
     */
   });
 
-  router.get('/buy', async (req, res, next) => {
-    return next('Not implemented');
+  router.get("/buy", async (req, res, next) => {
+    return next("Not implemented");
 
     /*
     try {
@@ -60,7 +60,7 @@ module.exports = () => {
       const user = res.locals.currentUser;
 
       // Get all basket items for a user
-      const basketItems = await basket.getAll(userId);
+      const basketItems = await basket.getAll();
 
       // be defensive
       if (!basketItems) {
@@ -80,8 +80,8 @@ module.exports = () => {
         // Create a new order and add all items
         await order.create(user, items, t);
         // Clear the users basket
-        await Promise.all(Object.keys(basketItems).map(async (key) => {
-          await basket.remove(key, userId);
+        await Promise.all(Object.keys(basketItems).map(async (itemId) => {
+          await basket.remove(key);
         }));
       });
 
