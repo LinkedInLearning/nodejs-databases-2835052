b09cfc7354537a53d3b8e68669950c86bcd1d99e
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-23 23:19:32 +0200

    Basket scaffold

--- shopper/server/routes/admin/item/index.js
+++ shopper/server/routes/admin/item/index.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 
-const ItemService = require('../../../services/ItemService');
+const ItemService = require("../../../services/ItemService");
 
 module.exports = () => {
   const router = express.Router();
 
-  router.get('/:itemId?', async (req, res, next) => {
-
+  router.get("/:itemId?", async (req, res, next) => {
     try {
       const items = await ItemService.getAll();
       let item = null;
@@ -16,7 +15,7 @@ module.exports = () => {
         item = await ItemService.getOne(req.params.itemId);
       }
 
-      return res.render('admin/item', {
+      return res.render("admin/item", {
         items,
         item,
       });
@@ -26,7 +25,7 @@ module.exports = () => {
   });
 
   // Save or update item
-  router.post('/', async (req, res) => {
+  router.post("/", async (req, res) => {
     // Massage the passed in form data a bit
     const sku = req.body.sku.trim();
     const name = req.body.name.trim();
@@ -35,10 +34,10 @@ module.exports = () => {
     // Make sure that the passed data is complete
     if (!sku || !name || !price) {
       req.session.messages.push({
-        type: 'warning',
-        text: 'Please enter SKU, name and price!',
+        type: "warning",
+        text: "Please enter SKU, name and price!",
       });
-      return res.redirect('/admin/item');
+      return res.redirect("/admin/item");
     }
 
     try {
@@ -54,40 +53,41 @@ module.exports = () => {
         await ItemService.update(req.body.itemId, itemData);
       }
       req.session.messages.push({
-        type: 'success',
-        text: `The item was ${req.body.itemId ? 'updated' : 'created'} successfully!`,
+        type: "success",
+        text: `The item was ${
+          req.body.itemId ? "updated" : "created"
+        } successfully!`,
       });
-      return res.redirect('/admin/item');
+      return res.redirect("/admin/item");
     } catch (err) {
       req.session.messages.push({
-        type: 'danger',
-        text: 'There was an error while saving the item!',
+        type: "danger",
+        text: "There was an error while saving the item!",
       });
       console.error(err);
-      return res.redirect('/admin/item');
+      return res.redirect("/admin/item");
     }
   });
 
   // Delete item
-  router.get('/delete/:itemId', async (req, res) => {
-
+  router.get("/delete/:itemId", async (req, res) => {
     try {
-      await ItemService.remove({ _id: req.params.itemId });
+      await ItemService.remove(req.params.itemId);
     } catch (err) {
       // Error handling
       req.session.messages.push({
-        type: 'danger',
-        text: 'There was an error while deleting the item!',
+        type: "danger",
+        text: "There was an error while deleting the item!",
       });
       console.error(err);
-      return res.redirect('/admin/item');
+      return res.redirect("/admin/item");
     }
     // Let the item knows that everything went fine
     req.session.messages.push({
-      type: 'success',
-      text: 'The item was successfully deleted!',
+      type: "success",
+      text: "The item was successfully deleted!",
     });
-    return res.redirect('/admin/item');
+    return res.redirect("/admin/item");
   });
   return router;
 };
