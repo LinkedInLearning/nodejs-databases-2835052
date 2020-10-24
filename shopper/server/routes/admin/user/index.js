2837b50be05e4ed05e7872d2870320f540ea1560
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-24 23:38:38 +0200

    update scaffold

--- shopper/server/routes/admin/user/index.js
+++ shopper/server/routes/admin/user/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 
-const UserService = require('../../../services/UserService');
+const UserService = require("../../../services/UserService");
 
 module.exports = () => {
   const router = express.Router();
 
-  router.get('/:userId?', async (req, res, next) => {
+  router.get("/:userId?", async (req, res, next) => {
     try {
       const users = await UserService.getAll();
 
@@ -15,7 +15,7 @@ module.exports = () => {
       if (req.params.userId) {
         user = await UserService.getOne(req.params.userId);
       }
-      return res.render('admin/user', {
+      return res.render("admin/user", {
         users,
         user,
       });
@@ -25,16 +25,16 @@ module.exports = () => {
   });
 
   // Save or update user
-  router.post('/', async (req, res) => {
+  router.post("/", async (req, res) => {
     const email = req.body.email.trim();
     const password = req.body.password.trim();
     // Add this here because on update we might want to keep the password as it is
     if (!email || (!password && !req.body.userId)) {
       req.session.messages.push({
-        type: 'warning',
-        text: 'Please enter email address and password!',
+        type: "warning",
+        text: "Please enter email address and password!",
       });
-      return res.redirect('/admin/user');
+      return res.redirect("/admin/user");
     }
     try {
       // If there was no existing user we now want to create a new user object
@@ -51,49 +51,50 @@ module.exports = () => {
         await UserService.update(req.body.userId, userData);
       }
       req.session.messages.push({
-        type: 'success',
-        text: `The user was ${req.body.userId ? 'updated' : 'created'} successfully!`,
+        type: "success",
+        text: `The user was ${
+          req.body.userId ? "updated" : "created"
+        } successfully!`,
       });
-      return res.redirect('/admin/user');
+      return res.redirect("/admin/user");
     } catch (err) {
       req.session.messages.push({
-        type: 'danger',
-        text: 'There was an error while saving the user!',
+        type: "danger",
+        text: "There was an error while saving the user!",
       });
       console.error(err);
-      return res.redirect('/admin/user');
+      return res.redirect("/admin/user");
     }
   });
 
   // Delete user
-  router.get('/delete/:userId', async (req, res) => {
+  router.get("/delete/:userId", async (req, res) => {
     try {
       await UserService.remove(req.params.userId);
     } catch (err) {
       // Error handling
       req.session.messages.push({
-        type: 'danger',
-        text: 'There was an error while deleting the user!',
+        type: "danger",
+        text: "There was an error while deleting the user!",
       });
       console.error(err);
-      return res.redirect('/admin/user');
+      return res.redirect("/admin/user");
     }
     // Let the user knows that everything went fine
     req.session.messages.push({
-      type: 'success',
-      text: 'The user was successfully deleted!',
+      type: "success",
+      text: "The user was successfully deleted!",
     });
-    return res.redirect('/admin/user');
+    return res.redirect("/admin/user");
   });
 
-
-  router.get('/impersonate/:userId', (req, res) => {
+  router.get("/impersonate/:userId", (req, res) => {
     req.session.userId = req.params.userId;
     req.session.messages.push({
-      type: 'success',
-      text: 'User successfully switched',
+      type: "success",
+      text: "User successfully switched",
     });
-    return res.redirect('/admin/user');
+    return res.redirect("/admin/user");
   });
 
   return router;
