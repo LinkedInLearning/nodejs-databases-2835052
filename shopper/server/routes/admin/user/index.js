53ca1543f82c2b4e325603ae042996c8623081c5
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-24 23:38:38 +0200

    update scaffold

--- shopper/server/routes/admin/user/index.js
+++ shopper/server/routes/admin/user/index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 
 module.exports = () => {
   const router = express.Router();
 
-  router.get('/:userId?', async (req, res) => {
-    return res.render('admin/user');
+  router.get("/:userId?", async (req, res) => {
+    return res.render("admin/user");
 
     /*
     try {
@@ -16,7 +16,7 @@ module.exports = () => {
       if (req.params.userId) {
         user = await UserService.getOne(req.params.userId);
       }
-      return res.render('admin/user', {
+      return res.render("admin/user", {
         users,
         user,
       });
@@ -27,8 +27,8 @@ module.exports = () => {
   });
 
   // Save or update user
-  router.post('/', async (req, res, next) => {
-    return next('Not implemented');
+  router.post("/", async (req, res, next) => {
+    return next("Not implemented");
 
     /*
     const email = req.body.email.trim();
@@ -36,10 +36,10 @@ module.exports = () => {
     // Add this here because on update we might want to keep the password as it is
     if (!email || (!password && !req.body.userId)) {
       req.session.messages.push({
-        type: 'warning',
-        text: 'Please enter email address and password!',
+        type: "warning",
+        text: "Please enter email address and password!",
       });
-      return res.redirect('/admin/user');
+      return res.redirect("/admin/user");
     }
     try {
       // If there was no existing user we now want to create a new user object
@@ -56,55 +56,58 @@ module.exports = () => {
         await UserService.update(req.body.userId, userData);
       }
       req.session.messages.push({
-        type: 'success',
-        text: `The user was ${req.body.userId ? 'updated' : 'created'} successfully!`,
+        type: "success",
+        text: `The user was ${
+          req.body.userId ? "updated" : "created"
+        } successfully!`,
       });
-      return res.redirect('/admin/user');
+      return res.redirect("/admin/user");
     } catch (err) {
       req.session.messages.push({
-        type: 'danger',
-        text: 'There was an error while saving the user!',
+        type: "danger",
+        text: "There was an error while saving the user!",
       });
       console.error(err);
-      return res.redirect('/admin/user');
+      return res.redirect("/admin/user");
     }
     */
   });
 
   // Delete user
-  router.get('/delete/:userId', async (req, res, next) => {
-    return next('Not implemented');
+  router.get("/delete/:userId", async (req, res, next) => {
+    return next("Not implemented");
+
     /*
     try {
       await UserService.remove(req.params.userId);
     } catch (err) {
       // Error handling
       req.session.messages.push({
-        type: 'danger',
-        text: 'There was an error while deleting the user!',
+        type: "danger",
+        text: "There was an error while deleting the user!",
       });
       console.error(err);
-      return res.redirect('/admin/user');
+      return res.redirect("/admin/user");
     }
     // Let the user knows that everything went fine
     req.session.messages.push({
-      type: 'success',
-      text: 'The user was successfully deleted!',
+      type: "success",
+      text: "The user was successfully deleted!",
     });
-    return res.redirect('/admin/user');
+    return res.redirect("/admin/user");
     */
   });
 
+  router.get("/impersonate/:userId", (req, res, next) => {
+    return next("Not implemented");
 
-  router.get('/impersonate/:userId', (req, res, next) => {
-    return next('Not implemented');
     /*
     req.session.userId = req.params.userId;
     req.session.messages.push({
-      type: 'success',
-      text: 'User successfully switched',
+      type: "success",
+      text: "User successfully switched",
     });
-    return res.redirect('/admin/user');
+    return res.redirect("/admin/user");
     */
   });
 
