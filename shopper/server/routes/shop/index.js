bb41d9fdf349b857abe2e234100a1add80a1976b
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-24 23:38:38 +0200

    update scaffold

--- shopper/server/routes/shop/index.js
+++ shopper/server/routes/shop/index.js
@@ -1,44 +1,47 @@
-const express = require('express');
-
+const express = require("express");
 
 module.exports = () => {
   const router = express.Router();
 
-  router.get('/', async (req, res) => {
-    return res.render('shop', {});
+  router.get("/", async (req, res) => {
+    return res.render("shop", {});
     /*
     const items = await ItemService.getAll();
     return res.render('shop', { items });
     */
   });
 
-  router.get('/tobasket/:itemId', async (req, res, next) => {
-    return next('Not implemented');
+  router.get("/tobasket/:itemId", async (req, res, next) => {
+    return next("Not implemented");
 
     /*
     if (!res.locals.currentUser) {
       req.session.messages.push({
-        type: 'warning',
-        text: 'Please log in first',
+        type: "warning",
+        text: "Please log in first",
       });
-      return res.redirect('/shop');
+      return res.redirect("/shop");
     }
 
     try {
-      await basket.add(req.params.itemId, res.locals.currentUser.id);
+      const basket = new BasketService(
+        config.redis.client,
+        res.locals.currentUser.id
+      );
+      await basket.add(req.params.itemId);
       req.session.messages.push({
-        type: 'success',
-        text: 'The item was added to the basket',
+        type: "success",
+        text: "The item was added to the basket",
       });
     } catch (err) {
       req.session.messages.push({
-        type: 'danger',
-        text: 'There was an error adding the item to the basket',
+        type: "danger",
+        text: "There was an error adding the item to the basket",
       });
       console.error(err);
     }
 
-    return res.redirect('/shop');
+    return res.redirect("/shop");
     */
   });
 
