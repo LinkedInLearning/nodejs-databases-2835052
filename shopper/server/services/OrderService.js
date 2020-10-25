94224662efae9d71345fd1f429cd1b5ccccfc20f
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-25 19:22:30 +0100

    add orderservice

--- shopper/server/services/OrderService.js
+++ shopper/server/services/OrderService.js
@@ -6,6 +6,51 @@ class OrderService {
     this.client = sequelize;
     this.models = sequelize.models;
   }
+
+  async inTransaction(work) {
+    const t = await this.client.transaction();
+    try {
+      await work(t);
+      return t.commit();
+    } catch (error) {
+      t.rollback();
+      throw error;
+    }
+  }
+
+  async create(user, items, t) {
+    const order = await this.models.Order.create(
+      {
+        userId: user.id,
+        email: user.email,
+        status: "Not Shipped",
+      },
+      { transaction: t }
+    );
+
+    return Promise.all(
+      items.map(async (item) => {
+        const orderItem = await this.models.OrderItem.create({
+          sku: item.sku,
+          qty: item.qty,
+          price: item.price,
+          name: item.name,
+        });
+        return order.addOrderItem(orderItem, { transaction: t });
+      })
+    );
+  }
+
+  async getAll() {
+    return this.models.Order.findAll({
+      where: {},
+      include: [this.models.OrderItem],
+    });
+  }
+
+  async setStatus(orderId, status) {
+    return this.models.Order.update({ status }, { where: { id: orderId } });
+  }
 }
 
 module.exports = OrderService;
