e20f8cf93d51662acc64f6a67e0579bd8c85bdcd
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-25 10:19:41 +0100

    update from redo

--- shopper/server/bin/start.js
+++ shopper/server/bin/start.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const http = require('http');
-const mongoose = require('mongoose');
+const http = require("http");
+const mongoose = require("mongoose");
 
-const config = require('../config');
-const App = require('../app');
+const config = require("../config");
+const App = require("../app");
 
 async function connectToMongoose() {
   return mongoose.connect(config.mongodb.url, {
@@ -17,24 +17,22 @@ async function connectToMongoose() {
 
 /* Logic to start the application */
 const app = App(config);
-const port = process.env.PORT || '3000';
-app.set('port', port);
+const port = process.env.PORT || "3000";
+app.set("port", port);
 
 function onError(error) {
-  if (error.syscall !== 'listen') {
+  if (error.syscall !== "listen") {
     throw error;
   }
-  const bind = typeof port === 'string'
-    ? `Pipe ${port}`
-    : `Port  ${port}`;
+  const bind = typeof port === "string" ? `Pipe ${port}` : `Port  ${port}`;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
-    case 'EACCES':
+    case "EACCES":
       console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
-    case 'EADDRINUSE':
+    case "EADDRINUSE":
       console.error(`${bind} is already in use`);
       process.exit(1);
       break;
@@ -46,18 +44,18 @@ function onError(error) {
 const server = http.createServer(app);
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? `pipe ${addr}`
-    : `port ${addr.port}`;
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr.port}`;
 
   console.info(`${config.applicationName} listening on ${bind}`);
 }
-server.on('error', onError);
-server.on('listening', onListening);
+server.on("error", onError);
+server.on("listening", onListening);
 
-connectToMongoose().then(() => {
-  console.info('Successfully connected to MongoDB');
-  server.listen(port);
-}).catch((error) => {
-  console.error(error);
-});
+connectToMongoose()
+  .then(() => {
+    console.info("Successfully connected to MongoDB");
+    server.listen(port);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
