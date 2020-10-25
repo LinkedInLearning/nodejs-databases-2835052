bf1a9c076659c5c4eab7244d9f1535bf7f96451e
LinkedIn Author <author@rmbp-dkhan.local>
2020-10-25 20:40:00 +0100

    finish order page

--- shopper/server/routes/admin/orders/index.js
+++ shopper/server/routes/admin/orders/index.js
@@ -1,51 +1,45 @@
-const express = require('express');
+const express = require("express");
+const OrderService = require("../../../services/OrderService");
 
-module.exports = () => {
+module.exports = (config) => {
   const router = express.Router();
 
-  router.get('/', async (req, res) => {
-    return res.render('admin/orders', {});
+  const order = new OrderService(config.mysql.client);
 
-    /*
+  router.get("/", async (req, res, next) => {
     try {
       // Get all orders
       const orderResult = await order.getAll();
       // Run map on the data to convert it into nested arrays with orders and orderitems
-      const orders = orderResult.map(item => item.get({ plain: true }));
-      return res.render('admin/orders', { orders });
+      const orders = orderResult.map((item) => item.get({ plain: true }));
+      return res.render("admin/orders", { orders });
     } catch (err) {
       req.session.messages.push({
-        type: 'danger',
-        text: 'There was an error fetching the orders',
+        type: "danger",
+        text: "There was an error fetching the orders",
       });
       console.error(err);
       return next(err);
     }
-    */
   });
 
-  router.get('/setshipped/:orderId', async (req, res, next) => {
-    return next('Not Implemented');
-
-    /*
+  router.get("/setshipped/:orderId", async (req, res) => {
     try {
-      await order.setStatus(req.params.orderId, 'Shipped');
+      await order.setStatus(req.params.orderId, "Shipped");
       req.session.messages.push({
-        type: 'success',
-        text: 'Status updated',
+        type: "success",
+        text: "Status updated",
       });
-      return res.redirect('/admin/orders');
+      return res.redirect("/admin/orders");
     } catch (err) {
       req.session.messages.push({
-        type: 'danger',
-        text: 'There was an updaeting the order',
+        type: "danger",
+        text: "There was an updaeting the order",
       });
       console.error(err);
-      return res.redirect('/admin/orders');
+      return res.redirect("/admin/orders");
     }
-    */
   });
 
   return router;
 };
-
--- shopper/server/routes/basket/index.js
+++ shopper/server/routes/basket/index.js
@@ -65,9 +65,7 @@ module.exports = (config) => {
     return res.redirect("/basket");
   });
 
-  router.get("/buy", async (req, res, next) => {
-    return next("Not implemented");
-    /*
+  router.get("/buy", async (req, res) => {
     if (!res.locals.currentUser) {
       req.session.messages.push({
         type: "warning",
@@ -128,7 +126,6 @@ module.exports = (config) => {
       console.error(err);
       return res.redirect("/basket");
     }
-    */
   });
 
   return router;
