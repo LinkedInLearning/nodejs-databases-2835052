75196523371646e73210f69153591eeb02c631a4
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2023-12-18 12:01:37 -0500

    Connect to mongoDB

--- maxcoin/main.js
+++ maxcoin/main.js
@@ -1,12 +1,15 @@
-const CoinAPI = require("./services/CoinAPI");
+// const CoinAPI = require("./services/CoinAPI");
+const MongoBackend=require('./services/backend/MongoBackend')
 
 async function run() {
-  const coinAPI = new CoinAPI();
-  return coinAPI.fetch();
+  // const coinAPI = new CoinAPI();
+  // return coinAPI.fetch();
+const mongoBackend=new MongoBackend()
+return mongoBackend.max()
 }
 
 run()
   .then((result) => {
-    console.log(result);
+    console.log('result:',result);
   })
   .catch((err) => console.error(err));
--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -1,23 +1,57 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
-const CoinAPI = require('../CoinAPI');
+
+// const CoinAPI = require('../CoinAPI');
+const { MongoClient } = require('mongodb')
 
 class MongoBackend {
-
   constructor() {
-    this.coinAPI = new CoinAPI();
+    // this.coinAPI = new CoinAPI();
+    this.mongoUrl = 'mongodb://localhost:37017/maxcooin'
+    this.client = null
+    this.collection = null
   }
 
-  async connect() {}
+  async connect() {
+    const mongoClient = new MongoClient(this.mongoUrl, {
+      useUnifiedTopology: true, // useNewUrlParser is a deprecated option: useNewUrlParser has no effect since Node.js Driver version 4.0.0 and will be removed in the next major version
+      useNewUrlParser: true, // useUnifiedTopology is a deprecated option: useUnifiedTopology has no effect since Node.js Driver version 4.0.0 and will be removed in the next major version
+    })
 
-  async disconnect() {}
+    this.client = await mongoClient.connect()
+
+    this.collection = this.client.db('maxcoin').collection('values')
+
+    return this.client
+  }
+
+  async disconnect() {
+    if (this.client) return this.client.close()
+
+    return false
+  }
 
   async insert() {}
 
   async getMax() {}
 
-  async max() {}
+  async max() {
+    console.info('### Connection to MongoDB')
+    console.time('--- mongodb-connect')
+    const client = await this.connect()
+    if (client.isConnected()) {
+      console.info('Successfully connected to MongoDB')
+    } else {
+      throw new Error('Connection to MongoDB failed!')
+    }
+    console.timeEnd('--- mongodb-connect')
+
+    console.info('### Disconnection from MongoDB')
+    console.time('--- mongodb-disconnect')
+    await this.disconnect()
+    console.timeEnd('--- mongodb-disconnect')
+  }
 }
 
-module.exports = MongoBackend;
+module.exports = MongoBackend
