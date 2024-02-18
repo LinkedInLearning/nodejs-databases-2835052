23e5b9440b590bf61cc015cf1f42251b3b2bf42d
taha-harb <harb_taha@hotmail.com>
2024-02-18 21:43:30 +0200

    update

--- maxcoin/services/backend/MongoBackend.js
+++ maxcoin/services/backend/MongoBackend.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-useless-constructor */
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-empty-function */
-const CoinAPI = require('../CoinAPI');
+const CoinAPI = require("../CoinAPI");
 
 class MongoBackend {
-
   constructor() {
     this.coinAPI = new CoinAPI();
+    this.client = null;
+    this.collection = null;
+    this.mongoUrl = "mongodb://localhost:37017/maxcoin";
   }
 
   async connect() {}
